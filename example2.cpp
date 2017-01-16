11f8f0573910dda787dedbe7509664fb25318a43
Andreas Röver <roever@users.sf.net>
2017-01-16 20:52:01 +0100

    simplify example 2 and provide more comments

--- example2.cpp
+++ example2.cpp
@@ -1,43 +1,61 @@
 #include "bsptree.hpp"
 
-#include <boost/qvm/vec_access.hpp>
-#include <boost/qvm/vec_traits_array.hpp>
+#include <boost/qvm/vec.hpp>
+#include <boost/qvm/vec_operations.hpp>
 
 #include <vector>
 
-// similar to example 1 but this time we directly use qvm::vert as type to avoid
-// registering the return type... probably the best solution if you can do it
+// This is the shortest example I can come up with, it creates a BSP tree from a set of
+// triangles and sorts them according to a vantage point
+
+using namespace boost::qvm;
+
+// This is the vertex structure we use in this example. You are free to use whatever
+// structure you want, with whatever content. But for demonstration purposes we keep
+// is to the essentials. The only required member for the BSP tree is the position of
+// the vertices, so we stay with it.
+// Also this library needs the position to be compatible with boost::qvm. We don't want
+// to bribe qvm into using our own type, so we stick with the qvm types for this example
+// see a later example for a possibility of your own struct
 struct Vertex
 {
-  boost::qvm::vec<float, 3> pos;
-
-  Vertex(float x1, float y1, float z1) : pos {x1, y1, z1} {}
-  Vertex(const boost::qvm::vec<float, 3> & p) : pos(p) {}
-  Vertex() : pos {0, 0, 0} {}
-
-
+  vec<float, 3> pos;
 };
 
-namespace bsp {
-
+// You always have to create this specialization for your Vertex type. This allows the
+// library to access the position and to create intermediate vertices, if needed. There
+// are 3 members in this struct
+namespace bsp
+{
   template <> struct bsp_vertex_traits<Vertex>
   {
-    typedef const boost::qvm::vec<float, 3> & position_type;
-    static position_type getPosition(const Vertex & v)
+    // the position type is the type you want the library to use to handle positions.
+    // optimally this is identical to your own position type in the Vertex structure,
+    // but it doesn't need to be
+    typedef vec<float, 3> position_type;
+
+    // getPosition is the function used by the library to read the position field from
+    // a vertex, you can return a copy or a const reference... it is up to you
+    static const position_type & getPosition(const Vertex & v)
     {
       return v.pos;
     }
+
+    // this function creates an interpolated vertex, it is intermediate between the
+    // two given vertices a and b
+    // the example implementation only has to care for the position, if your Vertex
+    // structure contains more fields you have to interpolate them as well, or not
+    // (e.g. when the colour is flat you don't)
     static Vertex getInterpolatedVertex(const Vertex & a, const Vertex & b, float i)
     {
-      using boost::qvm::operator+;
-      using boost::qvm::operator*;
-      return Vertex(a.pos*(1-i) + b.pos*i);
+      return { a.pos*(1-i) + b.pos*i };
     }
   };
 }
 
 int main()
 {
+  // simple example, mesh of a cube
   std::vector<Vertex> v2 {
     {0, 0, 0},{1, 0, 0},{1, 1, 0},    {0, 0, 0}, {1, 1, 0}, {0, 1, 0},
     {0, 0, 1},{1, 1, 1},{1, 0, 1},    {0, 0, 1}, {0, 1, 1}, {1, 1, 1},
@@ -47,23 +65,11 @@ int main()
 
     {0, 0, 0},{0, 1, 0},{0, 1, 1},    {0, 0, 0}, {0, 1, 1}, {0, 0, 1},
     {1, 0, 0},{1, 1, 1},{1, 1, 0},    {1, 0, 0}, {1, 0, 1}, {1, 1, 1},
-
-    {0.5, 0.5, 0.5},{1.5, 0.5, 0.5},{1.5, 1.5, 0.5},    {0.5, 0.5, 0.5}, {1.5, 1.5, 0.5}, {0.5, 1.5, 0.5},
-    {0.5, 0.5, 1.5},{1.5, 1.5, 1.5},{1.5, 0.5, 1.5},    {0.5, 0.5, 1.5}, {0.5, 1.5, 1.5}, {1.5, 1.5, 1.5},
-
-    {0.5, 0.5, 0.5},{1.5, 0.5, 1.5},{1.5, 0.5, 0.5},    {0.5, 0.5, 0.5}, {0.5, 0.5, 1.5}, {1.5, 0.5, 1.5},
-    {0.5, 1.5, 0.5},{1.5, 1.5, 0.5},{1.5, 1.5, 1.5},    {0.5, 1.5, 0.5}, {1.5, 1.5, 1.5}, {0.5, 1.5, 1.5},
-
-    {0.5, 0.5, 0.5},{0.5, 1.5, 0.5},{0.5, 1.5, 1.5},    {0.5, 0.5, 0.5}, {0.5, 1.5, 1.5}, {0.5, 0.5, 1.5},
-    {1.5, 0.5, 0.5},{1.5, 1.5, 1.5},{1.5, 1.5, 0.5},    {1.5, 0.5, 0.5}, {1.5, 0.5, 1.5}, {1.5, 1.5, 1.5},
   };
 
-  std::vector<uint8_t> indices(v2.size());
-  for (size_t i = 0; i < indices.size(); i++) indices[i] = i;
+  // create the tree
+  bsp::BspTree<std::vector<Vertex>, std::vector<uint8_t>> bsp2(std::move(v2));
 
-  // as we know that we only have very few vertices, we use uint8_t indices this time... this limits us
-  // to 256 vertices though but saves some memory and would require less upload to the GPU
-  bsp::BspTree<std::vector<Vertex>, std::vector<uint8_t>> bsp2(std::move(v2), indices);
-
-  auto a2 = bsp2.sort(boost::qvm::vec<float, 3>{-5, 5, 5});
+  // sort the triangles
+  auto a2 = bsp2.sort(vec<float, 3>{-5, 5, 5});
 }
